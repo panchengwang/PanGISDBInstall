4f70a79c3bb18976de2b0cad7427d4d82dbc5461
pcwang <wang_wang_lao@163.com>
2023-06-12 16:17:55 +0800

    postgis需要下载tar.gz源码

--- script/ubuntu20.04/download_src.sh
+++ script/ubuntu20.04/download_src.sh
@@ -15,6 +15,7 @@ GEOTIFF_VERSION=master
 
 echo "正在下载postgresql-${PGSQL_VERSION}.tar.bz2, 请等待..."
 wget -c "https://ftp.postgresql.org/pub/source/v${PGSQL_VERSION}/postgresql-${PGSQL_VERSION}.tar.bz2"
+rm -rf postgresql
 tar -jvxf `ls postgresql*.tar.bz2`
 mv `ls -d */ | grep "postgresql" | sed "s/\///"` postgresql
 echo "postgresql-${PGSQL_VERSION}.tar.bz2下载成功"
@@ -31,9 +32,17 @@ echo "从github下载geos 版本分支: ${GEOS_VERSION}, 请等待..."
 git clone https://github.com/libgeos/geos.git -b ${GEOS_VERSION}
 echo "geos下载成功"
 
-echo "从github下载postgis 版本分支: ${POSTGIS_VERSION}, 请等待..."
-git clone https://github.com/postgis/postgis.git -b ${POSTGIS_VERSION}
-echo "postgis下载成功"
+# echo "从github下载postgis 版本分支: ${POSTGIS_VERSION}, 请等待..."
+# git clone https://git.osgeo.org/gitea/postgis/postgis.git -b ${POSTGIS_VERSION}
+# echo "postgis下载成功"
+
+echo "postgis的github代码没有定义好control文件，使用create extension出错"
+echo "需要下载https://download.osgeo.org/postgis/source/postgis-3.3.3.tar.gz"
+wget -c https://download.osgeo.org/postgis/source/postgis-3.3.3.tar.gz
+rm -rf postgis
+tar -zvxf `ls postgis*.tar.gz`
+mv `ls -d */ | grep "postgis" | sed "s/\///"` postgis
+echo 
 
 echo "从github下载libgeotiff 版本分支: ${GEOTIFF_VERSION}, 请等待..."
 git clone https://github.com/OSGeo/libgeotiff.git -b ${GEOTIFF_VERSION}
